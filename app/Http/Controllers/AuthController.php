72a018b92ceb5d73a1252858723b9dab132b08cd
LesterOsana18 <lesterfosana@iskolarngbayan.pup.edu.ph>
2025-06-09 17:34:36 +0800

    feat(auth): add success message on user registration redirect

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -56,7 +56,7 @@ class AuthController extends Controller
 
         Auth::login($user);
 
-        return redirect()->route('home');
+        return redirect()->route('home')->with('success', 'Welcome to Commeownity, ' . $user->first_name . '!');
     }
 
     public function showLoginForm()
