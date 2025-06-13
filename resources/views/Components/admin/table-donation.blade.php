b21947e41af0f16461f147c514f6aa43214c4122
LesterOsana18 <lesterfosana@iskolarngbayan.pup.edu.ph>
2025-06-13 16:51:42 +0800

    refactor(Donation table): remove unused 'Add Record' button for cleaner UI

--- resources/views/Components/admin/table-donation.blade.php
+++ resources/views/Components/admin/table-donation.blade.php
@@ -7,10 +7,6 @@
             <div class="rounded-lg px-4 py-2 bg-[#502C58]">
                 Donations
             </div>
-            <button class="rounded-lg px-4 py-2 bg-[#4ABDAC] hover:bg-[#369688] flex items-center">
-                <i class="fa-solid fa-plus mr-2"></i>
-                Add Record
-            </button>
         </div>
         <table class="table-fixed min-w-full border border-collapse text-sm">
             <thead>
