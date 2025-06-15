3ddd6b549d231e91fd7509ba73581852825ff226
pau-snchz <96534030+pau-snchz@users.noreply.github.com>
2025-06-15 21:28:50 +0800

    feat(event-modal): removed notifications

--- resources/views/livewire/event-modal.blade.php
+++ resources/views/livewire/event-modal.blade.php
@@ -77,23 +77,4 @@
             @endif
         </div>
     </div>
-
-    <!-- Notifications -->
-    @if(session('message'))
-        <div x-data="{ show: true }"
-            x-show="show"
-            x-init="setTimeout(() => show = false, 3000)"
-            class="fixed top-4 right-4 bg-green-500 text-white px-4 py-2 rounded-lg shadow-lg z-50">
-            {{ session('message') }}
-        </div>
-    @endif
-
-    @if(session('error'))
-        <div x-data="{ show: true }"
-            x-show="show"
-            x-init="setTimeout(() => show = false, 3000)"
-            class="fixed top-4 right-4 bg-red-500 text-white px-4 py-2 rounded-lg shadow-lg z-50">
-            {{ session('error') }}
-        </div>
-    @endif
 </div>
